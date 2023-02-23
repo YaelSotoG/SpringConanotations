ea449313af477f9fd30482c3bdc7b715d8a90f2b
Yael Soto <xyarock@gmail.com>
2023-02-23 01:21:43 -0600

    Autowired como generarlo

--- src/main/java/com/example/demo/ComercialExperimentado.java
+++ src/main/java/com/example/demo/ComercialExperimentado.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //esta es la anotacion
 @Component("ComercialExperimentado")
+//la anotacion tambien puede estar sin nombre y para hablarle se debe de llamar el nombre de la clase
+//pero con la primera letra en minuscula
+//@Component
 public class ComercialExperimentado implements Empleados {
 
 	@Override
@@ -14,7 +18,19 @@ public class ComercialExperimentado implements Empleados {
 	@Override
 	public String getInforme() {
 		// TODO Auto-generated method stub
-		return "esto es un informe generado por el comercial ";
+		//return "esto es un informe generado por el comercial ";
+		return nuevoInforme.getInformeFinanciero();
 	}
+	//esto es de @autowired
+	//para el autowired se debe de 
+	//1 crear clase + interfaz a inyectar (dependencia)
+	//2 Crear constructor para inyeccion en clase que lo solicite
+	//3 configurar la dependencia con @Autowired
+	
+	@Autowired
+	public ComercialExperimentado(CreacionInformeFinanciero nuevoInforme) {
+		this.nuevoInforme = nuevoInforme;
+	}
+	private CreacionInformeFinanciero nuevoInforme;
 
 }
---
+++ src/main/java/com/example/demo/InformeFinancieroTrim1.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+//esto para registrarlo en string
+@Component
+public class InformeFinancieroTrim1 implements CreacionInformeFinanciero {
+
+	@Override
+	public String getInformeFinanciero() {
+		// TODO Auto-generated method stub
+		return "Presentacion de informe financiero del trimestre 1";
+	}
+
+}
